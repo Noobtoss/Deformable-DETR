fc17b712df9483da33ad5bd97a84b4c03657cf61
Noobtoss <96134731+Noobtoss@users.noreply.github.com>
2023-02-18 18:41:12 +0100

    Update augmentations.py

--- util/augmentations.py
+++ util/augmentations.py
@@ -2,156 +2,148 @@ import random
 import numpy as np
 import torch
 
-class Albumentations:
-    
-    def __init__(self, size=640):
+class Albumentations: # Semmel
+    # YOLOv5 Albumentations class (optional, only used if package is installed)
+    def __init__(self):
         self.transform = None
-        self.transform_pre = None
-        prefix = colorstr('albumentations: ')
+
+        hyp = type('', (), {})()
+        hyp.semmel_flag = 7
+        hyp.semmel_prob = 0.05
+        
         try:
             import albumentations as A
-            check_version(A.__version__, '1.0.3', hard=True)  # version requirement
-            version = 7 # 0=standAug 1=noAug 2=selectAug 3=selectAug+ 4=standAugSelectAug+scaleAug 5=scaleAug 6=Dropout 7=Dropout+standAug
+            # hyp.semmel_flag # 0=standAug 1=noAug 2=selectAug 3=selectAug+ 4=standAugSelectAug+scaleAug 5=scaleAug 6=Dropout 7=Dropout+standAug
             
-            if version == 0:
+            if hyp.semmel_flag == 0:
                 
                 T = [
-                    A.RandomResizedCrop(height=size, width=size, scale=(0.8, 1.0), ratio=(0.9, 1.11), p=0.0),
-                    A.Blur(p=0.01),
-                    A.MedianBlur(p=0.01),
-                    A.ToGray(p=0.01),
-                    A.CLAHE(p=0.01),
+                    A.Blur(p=hyp.semmel_prob),
+                    A.MedianBlur(p=hyp.semmel_prob),
+                    A.ToGray(p=hyp.semmel_prob),
+                    A.CLAHE(p=hyp.semmel_prob),
                     A.RandomBrightnessContrast(p=0.0),
                     A.RandomGamma(p=0.0),
                     A.ImageCompression(quality_lower=75, p=0.0)]  # transforms
                 
-                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
+                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
             
-            if version == 1:
+            if hyp.semmel_flag == 1:
                 
                 T = [A.Blur(p=0.0)]
                 
-                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
+                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
             
-            if version == 2:
+            if hyp.semmel_flag == 2:
                 
-                T1 = [A.Equalize(p=0.2)]
-                T2 = [A.RGBShift(p=0.2, r_shift_limit=30, g_shift_limit=30, b_shift_limit=30)]
-                T3 = [A.HueSaturationValue(p=0.2, hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30)]
-                T4 = [A.ISONoise(p=0.2)]
-                T5 = [A.RandomBrightnessContrast(p=0.2, brightness_limit=0.3, contrast_limit=0.3)]
+                T1 = [A.Equalize(p=hyp.semmel_prob)]
+                T2 = [A.RGBShift(r_shift_limit=30, g_shift_limit=30, b_shift_limit=30, p=hyp.semmel_prob)]
+                T3 = [A.HueSaturationValue(hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30, p=hyp.semmel_prob)]
+                T4 = [A.ISONoise(p=hyp.semmel_prob)]
+                T5 = [A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, p=hyp.semmel_prob)]
                 
                 self.transform = [
-                            A.Compose(T1, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
-                            A.Compose(T2, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
-                            A.Compose(T2, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
-                            A.Compose(T3, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
-                            A.Compose(T4, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
-                            A.Compose(T5, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
+                            A.Compose(T1, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])),
+                            A.Compose(T2, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])),
+                            A.Compose(T2, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])),
+                            A.Compose(T3, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])),
+                            A.Compose(T4, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])),
+                            A.Compose(T5, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
                             ]
 
-            if version == 3:
+            if hyp.semmel_flag == 3:
                  
-                T1 = [A.Equalize(p=0.2)]
-                T2 = [A.RGBShift(p=0.4, r_shift_limit=30, g_shift_limit=30, b_shift_limit=30)]
-                T3 = [A.HueSaturationValue(p=0.4, hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30)]
-                T4 = [A.ISONoise(p=0.2)]
-                T5 = [A.RandomBrightnessContrast(p=0.2, brightness_limit=0.3, contrast_limit=0.3)]
+                T1 = [A.Equalize(p=hyp.semmel_prob)]
+                T2 = [A.RGBShift(r_shift_limit=30, g_shift_limit=30, b_shift_limit=30, p=hyp.semmel_prob*2)]
+                T3 = [A.HueSaturationValue(hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30, p=hyp.semmel_prob*2)]
+                T4 = [A.ISONoise(p=hyp.semmel_prob)]
+                T5 = [A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, p=hyp.semmel_prob)]
             
                 self.transform = [
-                            A.Compose(T1, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
-                            A.Compose(T2, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
-                            A.Compose(T2, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
-                            A.Compose(T3, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
-                            A.Compose(T4, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])),
-                            A.Compose(T5, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
+                            A.Compose(T1, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])),
+                            A.Compose(T2, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])),
+                            A.Compose(T2, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])),
+                            A.Compose(T3, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])),
+                            A.Compose(T4, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])),
+                            A.Compose(T5, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
                             ]
 
-            if version == 4:
+            if hyp.semmel_flag == 4:
 
                 T0 = [
-                    A.Blur(p=0.01),
-                    A.MedianBlur(p=0.01),
-                    A.ToGray(p=0.01),
-                    A.CLAHE(p=0.01),] #transforms
+                    A.Blur(p=hyp.semmel_prob),
+                    A.MedianBlur(p=hyp.semmel_prob),
+                    A.ToGray(p=hyp.semmel_prob),
+                    A.CLAHE(p=hyp.semmel_prob),] #transforms
                  
                 T = [
-                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=0.5),
-                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=0.5)]
+                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=hyp.semmel_prob*4),
+                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=hyp.semmel_prob*4)]
 
-                T1 = [A.Equalize(p=0.1)]
-                T2 = [A.RGBShift(p=0.2, r_shift_limit=30, g_shift_limit=30, b_shift_limit=30)]
-                T3 = [A.HueSaturationValue(p=0.2, hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30)]
-                T4 = [A.ISONoise(p=0.1)]
-                T5 = [A.RandomBrightnessT0+T+A.Contrast(p=0.1, brightness_limit=0.3, contrast_limit=0.3)]
+                T1 = [A.Equalize(p=hyp.semmel_prob)]
+                T2 = [A.RGBShift(r_shift_limit=30, g_shift_limit=30, b_shift_limit=30, p=hyp.semmel_prob*2)]
+                T3 = [A.HueSaturationValue(hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30, p=hyp.semmel_prob*2)]
+                T4 = [A.ISONoise(p=hyp.semmel_prob)]
+                T5 = [A.RandomBrightnessT0+T+A.Contrast(brightness_limit=0.3, contrast_limit=0.3, p=hyp.semmel_prob)]
 
                 self.transform = [
-                            A.Compose(T0+T+T1, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels'])),
-                            A.Compose(T0+T+T2, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels'])),
-                            A.Compose(T0+T+T2, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels'])),
-                            A.Compose(T0+T+T3, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels'])),
-                            A.Compose(T0+T+T4, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels'])),
-                            A.Compose(T0+T+T5, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels']))
+                            A.Compose(T0+T+T1, bbox_params=A.BboxParams(format='pascal_voc', min_area=100, label_fields=['class_labels'])),
+                            A.Compose(T0+T+T2, bbox_params=A.BboxParams(format='pascal_voc', min_area=100, label_fields=['class_labels'])),
+                            A.Compose(T0+T+T2, bbox_params=A.BboxParams(format='pascal_voc', min_area=100, label_fields=['class_labels'])),
+                            A.Compose(T0+T+T3, bbox_params=A.BboxParams(format='pascal_voc', min_area=100, label_fields=['class_labels'])),
+                            A.Compose(T0+T+T4, bbox_params=A.BboxParams(format='pascal_voc', min_area=100, label_fields=['class_labels'])),
+                            A.Compose(T0+T+T5, bbox_params=A.BboxParams(format='pascal_voc', min_area=100, label_fields=['class_labels']))
                             ]
 
-            if version == 5:
+            if hyp.semmel_flag == 5:
 
                 T = [
-                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=1.0),
-                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=1.0)]
+                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=hyp.semmel_prob*4),
+                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=hyp.semmel_prob*4)]
 
                 self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels']))
 
-            if version == 6:
+            if hyp.semmel_flag == 6:
 
-                T_pre = [A.CoarseDropout(max_holes=20, max_height=0.03, max_width=0.03, min_holes=10, min_height=0.01, min_width=0.01, fill_value=0, p=0.1)]
+                T_pre = [A.CoarseDropout(max_holes=50, max_height=0.1, max_width=0.1, min_holes=10, min_height=0.01, min_width=0.01, fill_value=0, p=hyp.semmel_prob*4)]
 
                 T = [
-                    A.PixelDropout(dropout_prob=0.05, per_channel=False, drop_value=0, p=0.1),
-                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=1.0),
-                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=1.0)
+                    A.PixelDropout(dropout_prob=0.05, per_channel=False, drop_value=0, p=hyp.semmel_prob*4),
+                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=hyp.semmel_prob*4),
+                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=hyp.semmel_prob*4)
                     ]
 
                 self.transform_pre = A.Compose(T_pre)
-                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels']))
+                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='pascal_voc', min_area=100, label_fields=['class_labels']))
                     
-            if version == 7:
+            if hyp.semmel_flag == 7:
                
-                T_pre = [A.CoarseDropout(max_holes=20, max_height=0.03, max_width=0.03, min_holes=10, min_height=0.01, min_width=0.01, fill_value=0, p=0.05)]
+                T_pre = [A.CoarseDropout(max_holes=50, max_height=0.1, max_width=0.1, min_holes=10, min_height=0.01, min_width=0.01, fill_value=0, p=hyp.semmel_prob*4)]
                 
                 T = [
-                    A.PixelDropout(dropout_prob=0.05, per_channel=False, drop_value=0, p=0.05),
-                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=0.2),
-                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=0.2),
-                    A.RandomResizedCrop(height=size, width=size, scale=(0.8, 1.0), ratio=(0.9, 1.11), p=0.0),
-                    A.Blur(p=0.05),
-                    A.MedianBlur(p=0.05),
-                    A.ToGray(p=0.05),
-                    A.CLAHE(p=0.05),
+                    A.PixelDropout(dropout_prob=0.05, per_channel=False, drop_value=0, p=hyp.semmel_prob*4),
+                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(-0.4, 0.1), rotate_limit=0, interpolation=1, border_mode=0, rotate_method='ellipse', p=hyp.semmel_prob*4),
+                    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=45, interpolation=1, border_mode=0, rotate_method='ellipse', p=hyp.semmel_prob*4),
+                    A.Blur(p=hyp.semmel_prob),
+                    A.MedianBlur(p=hyp.semmel_prob),
+                    A.ToGray(p=hyp.semmel_prob),
+                    A.CLAHE(p=hyp.semmel_prob),
                     A.RandomBrightnessContrast(p=0.0),
                     A.RandomGamma(p=0.0),
                     A.ImageCompression(quality_lower=75, p=0.0)
                     ]
 
                 self.transform_pre = A.Compose(T_pre)
-                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', min_area=100, label_fields=['class_labels']))
-                              
-            LOGGER.info(prefix + ', '.join(f'{x}'.replace('always_apply=False, ', '') for x in T if x.p))
+                self.transform = A.Compose(T, bbox_params=A.BboxParams(format='pascal_voc', min_area=100, label_fields=['class_labels']))
         except ImportError:#  # package not installed, skip
             pass
-        except Exception as e:
-            LOGGER.info(f'{prefix}{e}')
 
     def __call__(self, im, labels, p=1.0):
-
         if random.random() < p:
-
             if self.transform_pre:
                 new = self.transform_pre(image=im) # transformed
                 im = new['image']
-
             if self.transform:
-
                 if isinstance(self.transform, list):
                     new = random.choice(self.transform)(image=im, bboxes=labels[:, 1:], class_labels=labels[:, 0]) # transformed
                 else:
